f66efc6638a3c8c9e7941d1824af506e8e7e8f9f
LeVietHa05 <ha.leviet.0504@gmail.com>
2023-09-23 19:52:42 +0700

    2nd test: working well

--- app.js
+++ app.js
@@ -4,38 +4,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var app = express();
-var http = require("http");
-var server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server, {
-  allowEIO3: true,
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-    transports: ["websocket", "polling"],
-    credentials: true,
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log("[INFO] new connection: [" + socket.id + "]",
-    socket.request.connection.remoteAddress);
-  socket.on("message", (data) => {
-    console.log(`message from ${data.clientID} via socket id: ${socket.id}`);
-    socket.broadcast.emit("message", data);
-  });
-  /**************************** */
-  //xu ly chung
-  socket.on("reconnect", function () {
-    console.log("[" + socket.id + "] reconnect.");
-  });
-  socket.on("disconnect", () => {
-    console.log("[" + socket.id + "] disconnect.");
-  });
-  socket.on("connect_error", (err) => {
-    console.log(err.stack);
-  });
-})
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -50,4 +18,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = server;
+module.exports = app;
