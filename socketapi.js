843e0b857c0591485cb5fceaab50814a0b42ba00
LeVietHa05 <ha.leviet.0504@gmail.com>
2023-11-20 20:27:54 +0700

    add map

--- socketapi.js
+++ socketapi.js
@@ -3,12 +3,18 @@ const io = require("socket.io")();
 const socketapi = {
     io: io
 }
-
+let khoangcach = 0;
+let lastKhoangcach = 0;
 io.on("connection", (socket) => {
     console.log("[INFO] new connection: [" + socket.id + "]",
         socket.request.connection.remoteAddress);
     socket.on("message", (data) => {
         console.log(`message from ${data.clientID} via socket id: ${socket.id}`);
+        if (data.khoangcach > 0 && lastKhoangcach != data.khoangcach)  {
+            khoangcach += 60 * 3.14;
+            lastKhoangcach = data.khoangcach;
+            data.khoangcach = khoangcach;
+        }
         socket.broadcast.emit("message", data);
     });
     /**************************** */
